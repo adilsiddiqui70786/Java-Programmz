a658dc959647ccc6814ba077c4ae52384d87e483
adil <brokenaedil@gmail.com>
2022-12-01 23:34:33 +0530

    third commit

---
+++ strvalue.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class strvalue {
+    public static void main(String[] args) 
+
+    {
+        Scanner input = new Scanner(System.in);
+        int c=65;
+        System.out.println(c);
+    }
+    
+}  //store the integer 
+
