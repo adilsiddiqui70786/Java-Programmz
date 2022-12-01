2d444009b77c9f0285016dfc0c69d6feb0b24c2f
adil <brokenaedil@gmail.com>
2022-12-01 23:38:22 +0530

    sixth commit

---
+++ strchar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class strchar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char n = 'p';
+        System.out.println(n);
+
+    }
+           // STORING CHARACTER
+}
