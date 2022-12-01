48e7cd5c2de4ca7bd1560404f4285eff3f5f1650
adil <brokenaedil@gmail.com>
2022-12-01 23:37:19 +0530

    fivth commit

---
+++ strfloat.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class strfloat{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        float b=6.5f;
+        System.out.println(b);
+        
+    }
+            // STORING FLOAT VALUE
+}
