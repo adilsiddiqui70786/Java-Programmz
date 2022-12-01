39c772db63ee1c349fd34c46381418cb2b119e8c
adil <brokenaedil@gmail.com>
2022-12-01 23:41:46 +0530

    nine commit

---
+++ math.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class math {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Result Of the equation = ");
+        float c = 8/8+7%2-3*2f;
+        System.out.println(c);
+    }
+}       //RESULT OF THE GIVEN EQUATION...
