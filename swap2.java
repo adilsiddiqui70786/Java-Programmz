b567b59f03ea674559ebe02698f6635f73beda08
adil <brokenaedil@gmail.com>
2022-12-01 23:40:48 +0530

    eighth commit

---
+++ swap2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class swap2 {
+    
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = 10;
+        int b = 20;
+        System.out.println("swap the a and b");
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        
+        System.out.println("a="  +a);
+        System.out.println("b="  +b);
+        
+    }    //SWAPPING TWO INTEGER WITHOUT THIRD VARIABLE
+    
+}
