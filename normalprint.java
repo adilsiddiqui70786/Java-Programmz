aa59abe9ec2eae2f246aa326388a8227f7058223
adil <brokenaedil@gmail.com>
2022-12-01 23:28:02 +0530

    Second commit

---
+++ normalprint.java
@@ -0,0 +1,6 @@
+public class normalprint{
+    public static void main(String[] args) {
+        System.out.println("Mohd Adil❤️");
+        System.out.println( "BCA" +  "\n"  + "BCA all notez here");
+    }
+}      //FOR NORMAL PRINTING
