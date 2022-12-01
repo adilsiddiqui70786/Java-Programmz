47402bad8e731c2c719bd389344495b3192d4300
adil <brokenaedil@gmail.com>
2022-12-01 23:36:22 +0530

    fourth commit

---
+++ strinteger.java
@@ -0,0 +1,11 @@
+public class strinteger {
+    public static void main(String[] args) {
+        System.out.println("first integer value");
+        int a = 45;
+        System.out.println(a);
+        System.out.println("second integer value");
+        int b = 65;
+        System.out.println(b);
+    }
+    
+}         //STORING TWO INTEGERS
