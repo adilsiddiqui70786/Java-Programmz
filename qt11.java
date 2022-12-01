fc7e1a8fb4fae9187ec1362980fa3f609293039c
adil <brokenaedil@gmail.com>
2022-12-01 23:43:23 +0530

    eleven commit

---
+++ qt11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class qt11 {
+    public static void main(String[] args) {
+        Scanner pma = new Scanner(System.in);
+        System.out.println("1st number =");
+        int p = pma.nextInt();
+        System.out.println("2nd number =");
+        int m = pma.nextInt();
+        int a = p+m;
+        System.out.println("a = "  +a);
+    }
+}        //CALCULATE P AND M AND PRINT ITS SUM INTO A ..
