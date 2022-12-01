f305d5dd251d2dacb5eaf412aed606760626bf75
adil <brokenaedil@gmail.com>
2022-12-01 23:39:16 +0530

    seventh commit

---
+++ swap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+    Scanner pma = new Scanner(System.in);
+    int p = 7;
+    int m = 8;
+    System.out.println("swap p & m with a");
+    int a = p;
+    p = m;
+    m = a;
+    System.out.println("p="  +p);
+    System.out.println("m=" +m);    
+    m = a;   
+    }
+      // SWAP P AND M WITH THIRD VARIABLE A...
+}
