d25ab17d2bd945eb6f7c8cfd97a406fe2cf25865
adil <brokenaedil@gmail.com>
2022-12-03 11:49:15 +0530

    fifteen commit

---
+++ avgsum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class avgsum
+{
+    public static void main(String[] args) 
+    {
+        Scanner pma = new Scanner(System.in);
+        System.out.println("Enter the p and m value :");
+        float p = pma.nextInt();
+        float m = pma.nextInt();
+        float a = p+m;
+        float avgsum = (p+m)/2f;
+        System.out.println("sum" +a);
+        System.out.println("average" +avgsum);
+    }
+}        //FOR AVERAGE....
