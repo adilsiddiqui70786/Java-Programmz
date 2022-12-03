b566c6a2f866b5d1de54ff61970250273feb5661
adil <brokenaedil@gmail.com>
2022-12-03 11:33:30 +0530

    thirteen commit

---
+++ qt13.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class qt13 {
+    public static void main(String[] args) {
+        int p,m,a;
+        Scanner pma =new Scanner(System.in);
+        System.out.println("enter the value");
+        p = pma.nextInt();
+        m = pma.nextInt();
+        a= p+m;
+        System.out.println(+p +"+"+ +m +"="+ +a);
+        System.out.println(a);
+}
+}      // FOR ADDING TWO NUMBER
