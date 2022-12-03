65dd4d14734e7da3fd2ab66e06e13ee89cab782e
adil <brokenaedil@gmail.com>
2022-12-03 13:59:57 +0530

    twenty commit

---
+++ qt20.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class qt20 
+{
+    public static void main(String[] args) 
+    {
+        Scanner pma =new Scanner(System.in);
+        float a,b;
+        a= pma.nextFloat();
+        b= pma.nextFloat();
+        System.out.println();
+        System.out.println("enter two integer is " +a+ " and " +b);
+        System.out.println(+a+ " + " +b+ " = " +(a+b));
+        System.out.println(+a+ " - " +b+ " = " +(a-b));
+        System.out.println(+a+ " * " +b+ " = " +(a*b));
+        System.out.println(+a+ " / " +b+ " = " +(a/b));
+        System.out.println(+a+ " % " +b+ " = " +(a%b));
+    }
+}       //USE OF ADD , SUB , MULTIPLY , DIVIDE AND MOD...
