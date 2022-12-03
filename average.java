ff73bdd0d699a1a88d5546cb751ffa9c01bc5fa6
adil <brokenaedil@gmail.com>
2022-12-03 11:42:36 +0530

    fourteen commit

---
+++ average.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class average{
+    public static void main(String[] args) {
+        Scanner pma = new Scanner(System.in);
+        System.out.println("Enter the p and m value :");
+        float p = pma.nextInt();
+        float m = pma.nextInt();
+        float average = (p+m)/2f;
+        System.out.println("average" +average);
+    }
+}        
