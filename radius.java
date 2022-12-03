3dbffac0b65490e191af198d6649001b1a570e33
adil <brokenaedil@gmail.com>
2022-12-03 13:18:56 +0530

    eighteen commit

---
+++ radius.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class radius 
+{
+    public static void main(String[] args) {
+        Scanner pma = new Scanner(System.in);  //input from user
+        System.out.println("enter the radius of circle :"); //print
+        float p = pma.nextFloat();                               //float value
+        double pi = 3.14,area;                                 //double 
+        area  = pi*(p*p);
+        System.out.println("area of circle = " +area);
+        
+    }   // area of circle
+}
