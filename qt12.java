e48dbea861f10c0ae89ba3f89abfa05c2135c43e
adil <brokenaedil@gmail.com>
2022-12-03 11:27:58 +0530

    twelve commit

---
+++ qt12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class qt12 {
+    public static void main(String[] args) {
+        int p,m,a;
+        Scanner pma =new Scanner(System.in);
+        System.out.println("enter the value");
+        p = pma.nextInt();
+        m = pma.nextInt();
+        a= p+m;
+        System.out.println("sum=" +p +"+"+ +m +"="+ +a);
+        System.out.println(a);
+    }
+}        // for adding two integer
