084d1a1fbb55da2241270479d5cfcdd9c203602c
adil <brokenaedil@gmail.com>
2022-12-04 08:22:34 +0530

    twenty four commit

---
+++ sqrcbe2.java
@@ -0,0 +1,21 @@
+// sqaure and cube of numbers...
+import java.util.Scanner;
+public class sqrcbe2 
+{
+ public static void main(String[] args) 
+ {
+    Scanner pma = new Scanner(System.in);
+    System.out.println("Enter any integer = ");
+    int num = pma.nextInt();
+    if(num<5)
+    {
+        double square = num*num;
+        System.out.println("square of " +num + "is" +square);
+    }
+    else
+    {
+        double cube = num*num*num;
+        System.out.println("cube of" +num+ "is" +cube);
+    }   
+ }
+}
