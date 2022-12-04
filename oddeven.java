5c6b7856745b233eae6a7e2982329b166392b264
adil <brokenaedil@gmail.com>
2022-12-04 08:19:39 +0530

    twenty third commit

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+//CHECK THE NUMBER IS EVEN OR NOT..
+import java.util.Scanner;
+public class oddeven 
+{  public static void main(String[] args) {
+        System.out.println("Enter the integer numberz :");
+        Scanner pma = new Scanner(System.in);
+        int num = pma.nextInt();
+        if(num%2==0)
+        System.out.println("the integer is even");
+        else
+        System.out.println("the number is odd ");
+    }
+}
