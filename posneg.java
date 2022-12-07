29ef111a18863894746a20acd790568dbc8c1571
adil <brokenaedil@gmail.com>
2022-12-07 22:44:52 +0530

    twenty six commit

---
+++ posneg.java
@@ -0,0 +1,15 @@
+// find out the negative positive and zero integers...
+import java.util.Scanner;
+public class posneg {
+    public static void main(String[] args) {
+        Scanner pma = new Scanner(System.in);
+        System.out.println("Enter any integer");
+        int integer = pma.nextInt();
+        if(integer<0)
+        System.out.println("the integer is negative");
+        else if (integer>0)
+        System.out.println("the number is positive ");
+        else 
+        System.out.println("the integer is zero ");
+    }
+}
