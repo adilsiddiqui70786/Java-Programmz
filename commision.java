cc23e7eb8fabc6d3ec9c78ba5334ce1e3dfb266e
adil <brokenaedil@gmail.com>
2022-12-07 07:40:31 +0530

    twenty fifth commit

---
+++ commision.java
@@ -0,0 +1,19 @@
+// comissions
+import java.util.Scanner;
+public class commision {
+    public static void main(String[] args) 
+    {
+        Scanner  pma = new Scanner(System.in);
+        System.out.println("enter the sales = ");
+        int sale = pma.nextInt();
+        double commision ;
+        if(sale>=25000)
+        {
+            commision = (sale*5)/100;
+            System.out.println("commission = " +commision + "Rs..");
+        }
+        {
+            commision = (sale*7/100);
+            System.out.println("commission = " +commision + "Rs..");
+    }   }
+}
