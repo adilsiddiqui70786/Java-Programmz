7b759faebc677d8edef51cd23986dd6f112faecc
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2022-12-24 17:47:15 +0530

    10th commit

--- math2.java
+++ math2.java
@@ -1,12 +1,12 @@
+ //RESULTS OF THE EQUATION...
 import java.util.Scanner;
-
 public class math2 
 {
 public static void main(String[] args)
- {
+  {
     Scanner input = new Scanner(System.in);
     System.out.println("Result of the given equation = ");
     float a = 5%(5*2)-3+6/2f;
     System.out.println(a);
   }
-}     //RESULTS OF THE EQUATION...
+}    
