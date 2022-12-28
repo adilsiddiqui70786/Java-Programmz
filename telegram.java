5c0101d8f9d6720a629ea8fff3794315e426b100
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2022-12-28 10:19:59 +0530

    43th commit

--- telegram.java
+++ telegram.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
-
-public class telegram {
-    public static void main(String[] args) {
+public class telegram
+{
+    public static void main(String[] args) 
+    {
         Scanner pma = new Scanner(System.in);
         System.out.println("Enter the no. of words to be send by telegram : ");
         String words = pma.nextLine();
         if(words > 10);
             System.out.println("total charge is rs 7.5");
-        
-        else
-        
+        else   
             System.out.println("for each extra word , charge is rs 1.5 per word extra ");
-
-
     }
-}
+}           //CHARGES APPLY FOR WORDS...
