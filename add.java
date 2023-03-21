d9a16e004a74fb795ff4445ed41e7f70f71c787e
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-03-22 00:02:46 +0530

    Add two numbersa

---
+++ add.java
@@ -0,0 +1,20 @@
+
+
+import java.util.;
+class conditionif{
+public static void main (String args[])
+{
+  int rollnum;
+  System.out.println("Eneter the rollnum = ");
+  Scanner pma = new Scanner (System.in);
+  rollnum = pma.nextInt();
+  if(rollnum == 21)
+{
+  Sytem.out.println("My name is MOhd Adil"); 
+  Sytem.out.println("My age = 18");
+  Sytem.out.println("Contact - @adilsiddiqui70786");
+  Sytem.out.println("Pursuing BCA degree from Allahabad University");
+
+}
+}
+}
