0c9b1876f84295b3b0a778de64c6afe2133a3f65
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-03-22 00:03:11 +0530

    Do while loop

---
+++ dowhille.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class dowhile{
+
+    public class void main (String args[])
+
+{
+    int num;
+    System.out.println("Enter the number");
+    Scanner pma - new scanner (System.in);
+    num = pma.nextInt();
+
+    do 
+{
+   System.out.println(num+ " ");
+   ++num;
+}
+ 
+while(num<=10);
+}
+}
