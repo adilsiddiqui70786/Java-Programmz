b4ffa082e63583c72512badec7d4fceb948c2954
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-03-22 00:02:25 +0530

    Normal condition if

---
+++ conditionif.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class conditionif{
+public static void main (String args[])
+{
+  int rollnum;
+  System.out.println("Eneter the rollnum = ");
+  Scanner pma = new Scanner (System.in);
+  rollnum = pma.nextInt();
+  if(rollnum == 21)
+{
+  System.out.println("My name is MOhd Adil"); 
+  System.out.println("My age = 18");
+  System.out.println("Contact - @adilsiddiqui70786");
+  System.out.println("Pursuing BCA degree from Allahabad University");
+
+}
+}
+}
