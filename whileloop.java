b9a1c35a691e9b49518077d62852b0d9177adc16
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-03-22 00:01:53 +0530

    Using while loop

---
+++ whileloop.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class whileloop{
+
+     public static void main (String [] args)
+        {
+
+           int num;
+           System.out.println("Enter the number :");
+           Scanner pma = new Scanner (System.in);
+           num = pma.nextInt();
+      
+  while(num >= 0 )
+  {
+    
+         if(num%2==0)
+         {
+             System.out.println("Number is Even");
+             break;
+         }
+
+   else 
+        {
+           System.out.println("Number is Odd");
+           break;
+        }
+  }
+
+}
+}
