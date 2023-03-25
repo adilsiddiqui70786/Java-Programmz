d2100e3abcd3b7c2829e4e711038d8560bf37ffd
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-03-25 21:53:28 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -3,7 +3,7 @@ class abc
 {
     public static void main(String[] args) 
     {
-        System.out.println("Lo phir aa gye..");  
+        System.out.println("Swagat kro hamara ..");  
     }      
 }   
  
